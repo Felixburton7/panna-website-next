5dc60add63f881696c354061401284b1d8624652
Felix Burton <felixburton2002@gmail.com>
2024-12-30 23:07:57 +0000

    vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
-// src/App.js
+// // src/App.js
+// import React from "react";
+
+// import { Routes, Route } from "react-router-dom";
+// import { SpeedInsights } from "@vercel/speed-insights/react";
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
+// import Home from "./pages/Home";
+// import Games from "./pages/Games"; 
+// import TermsAndConditions from "./components/terms_and_conditions";
+
+// import About from "./pages/About";
+// import Responsibility from "./pages/Responsibility";
+// import GameRules from "./pages/GameRules";
+// import Careers from "./pages/Careers";
+// import Shop from "./pages/Shop";
+// import Partners from "./pages/Partners";
+// import { Helmet } from "react-helmet";
+
+
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <div className="app-container">
+//        <Helmet>
+//         <title>Panna</title>
+//       </Helmet>
+//       {/* Navigation Bar (always visible) */}
+//       <Navbar />
+
+//       {/* Main Content */}
+//       <div className="main-content">
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/games" element={<Games />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/responsibility" element={<Responsibility />} />
+//           <Route path="/rules" element={<GameRules />} />
+//           <Route path="/careers" element={<Careers />} />
+//           <Route path="/shop" element={<Shop />} />
+//           <Route path="/partners" element={<Partners />} />
+//           <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+
+//         </Routes>
+//         <SpeedInsights />
+
+//       </div>
+
+//       {/* Footer (always visible) */}
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react"; // Import Vercel Analytics
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import Games from "./pages/Games"; 
+import Games from "./pages/Games";
 import TermsAndConditions from "./components/terms_and_conditions";
-
 import About from "./pages/About";
 import Responsibility from "./pages/Responsibility";
 import GameRules from "./pages/GameRules";
@@ -16,13 +71,12 @@ import Shop from "./pages/Shop";
 import Partners from "./pages/Partners";
 import { Helmet } from "react-helmet";
 
-
 import "./App.css";
 
 function App() {
   return (
     <div className="app-container">
-       <Helmet>
+      <Helmet>
         <title>Panna</title>
       </Helmet>
       {/* Navigation Bar (always visible) */}
@@ -40,14 +94,15 @@ function App() {
           <Route path="/shop" element={<Shop />} />
           <Route path="/partners" element={<Partners />} />
           <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
-
         </Routes>
         <SpeedInsights />
-
       </div>
 
       {/* Footer (always visible) */}
       <Footer />
+
+      {/* Vercel Analytics */}
+      <Analytics />
     </div>
   );
 }
